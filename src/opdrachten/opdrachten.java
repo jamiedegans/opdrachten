d7ac2896515e162820ea2b11dd3d21e78afd2b5a
jamiedegans <1216596@student.roc-nijmegen.nl>
2025-11-20 12:06:36 +0100

    opdracht 2

--- src/opdrachten/opdrachten.java
+++ src/opdrachten/opdrachten.java
@@ -23,5 +23,28 @@ public class opdrachten
 	
 	}
 	
+//Opdracht 2:
+//	    Zet de eerste 50 getallen uit de fibonnaci reeks in een arraylist.
+//	    De reeks is: 0,1,1,2,3,5,8,13,….
+//	    Tip: Zet de eerste twee cijfers in de arraylist en vul de rest m.b.v. een for-lus.
+	ArrayList<Long> fbReeks = new ArrayList<Long>();
+	fbReeks.add(0l);
+	fbReeks.add(1l);
+	for(int i = 0; i <= 50; i++) {
+	    fbReeks.add(fbReeks.get(i) + fbReeks.get(i+1)); 
+	}
+	
+	for(int i = 0; i < fbReeks.size();i++) {
+	    System.out.println(fbReeks.get(i));
+	}
+	
+//	Opdracht 3:
+//
+//	    Schrijf een programma die test of de eerste en de laatste waarde in een array hetzelfde zijn.
+//
+//	    Experimenteer met arrays van verschillende lengte en inhoud.
+//
+//	    Denk na over hoe je dit zou moeten testen en maak een testset om de juistheid van je code aan te tonen.
+	
     }
 }
