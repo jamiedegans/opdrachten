053a0ca07a77ecd154f7db99d7788aa36704e43f
jamiedegans <1216596@student.roc-nijmegen.nl>
2025-11-20 11:37:30 +0100

    opdracht 1

--- src/opdrachten/opdrachten.java
+++ src/opdrachten/opdrachten.java
@@ -1,6 +1,27 @@
 package opdrachten;
 
+import java.util.ArrayList;
+
 public class opdrachten
 {
 
+    public static void main(String[] args)
+    {
+	//Opdracht 1:
+	// Vul een arraylist met alle even getallen onder de 100
+	// Gebruik hier een for-lus voor.
+	ArrayList<String> evenGetallen = new ArrayList<String>();
+
+	for (int i = 0; i < 100; i ++) {
+	    
+	   if (i % 2 == 0) { 
+		       System.out.println(i);
+	    
+	   } else {
+
+	   }
+	
+	}
+	
+    }
 }
